788d21ec1d2c5548b0af4a7bfaa3a29bf6071ebf
Gabe <gabeklav@fastmail.com>
2023-06-02 20:03:07 -0400

    Got basic 2 player results look down

--- src/results.ts
+++ src/results.ts
@@ -1,13 +1,52 @@
+import { isEqual } from "lodash";
 import { getSessionInfo } from "./api";
 import { getWordScore } from "./utils";
+import { DEBUG } from "./env";
 
 const urlParams = new URLSearchParams(window.location.search);
 export const SESSION_ID = urlParams.get("session");
 export const USER_ID = urlParams.get("user");
 
+const CHECK_UPDATE_INTERVAL_MS = 1000;
+
+let session: SessionView;
+let sortedBoardWords: string[];
+let wordsFoundByPlayers: Set<string>;
+let otherPlayers: {
+	[key: string]: ScoredPlayer;
+};
+let otherPlayerIdx = 0;
+
+setInterval(checkForUpdatedSession, CHECK_UPDATE_INTERVAL_MS);
+
+document.getElementById("left-button")?.addEventListener("pointerdown", (event) => {
+	if (otherPlayerIdx <= 0) {
+		console.warn("Somehow tried to reduce below first player");
+		return;
+	}
+
+	otherPlayerIdx--;
+	updateOtherPlayerColumn();
+});
+
+document.getElementById("right-button")?.addEventListener("pointerdown", (event) => {
+	if (otherPlayerIdx >= Object.keys(session.scoredUsers).length - 1) {
+		console.warn("Somehow tried to increase past the last player");
+		return;
+	}
+
+	otherPlayerIdx++;
+
+	updateOtherPlayerColumn();
+});
+
 async function init() {
 	const res = await getSessionInfo(SESSION_ID ?? "");
-	const session = (await res.json()) as SessionView;
+	session = (await res.json()) as SessionView;
+	if (DEBUG) {
+		console.debug("Game session:");
+		console.debug(session);
+	}
 
 	if (!session) {
 		console.error(`session not retrieved`);
@@ -23,10 +62,10 @@ async function init() {
 	}
 
 	// descending score order, else ascending alphabetical order
-	const sortedBoardWords = session.board.words.sort((a, b) =>
+	sortedBoardWords = session.board.words.sort((a, b) =>
 		a.length !== b.length ? b.length - a.length : a.localeCompare(b)
 	);
-	const wordsFoundByPlayers = new Set(
+	wordsFoundByPlayers = new Set(
 		Object.values(session.scoredUsers)
 			.map((scoredUser) => scoredUser.words)
 			.flat()
@@ -43,37 +82,47 @@ async function init() {
 		return;
 	}
 	(mainPlayerCol?.childNodes[1] as HTMLHeadingElement).innerHTML = mainPlayer.name;
-	const mainPlayerScore = mainPlayer.score;
-	(mainPlayerCol?.childNodes[3] as HTMLHeadingElement).innerHTML = mainPlayerScore
-		? mainPlayerScore.toString()
-		: "waiting...";
+	(mainPlayerCol?.childNodes[3] as HTMLHeadingElement).innerHTML =
+		mainPlayer.score !== undefined ? mainPlayer.score.toString() : "waiting...";
 
-	populateColumnWords(sortedBoardWords, wordsFoundByPlayers, mainPlayer, mainPlayerCol);
+	populateColumnWords(mainPlayer, mainPlayerCol);
 
-	delete session.scoredUsers[USER_ID];
+	otherPlayers = { ...session.scoredUsers };
+	delete otherPlayers[USER_ID];
+	if (DEBUG) {
+		console.debug("Other players:");
+		console.debug(otherPlayers);
+	}
 
 	// fill in the second col with some other player's score
-	let otherPlayerIdx = 0;
+	updateOtherPlayerColumn();
+}
+
+function updateOtherPlayerColumn() {
+	(document.getElementById("left-button") as HTMLButtonElement).disabled = otherPlayerIdx === 0;
+	(document.getElementById("right-button") as HTMLButtonElement).disabled =
+		otherPlayerIdx === Object.keys(otherPlayers).length - 1;
+
 	const otherPlayerCol = document.getElementById("otherPlayer");
 	if (!otherPlayerCol) {
 		console.error("Could not find other player column");
 		return;
 	}
-	const otherPlayer = Object.values(session.scoredUsers)[otherPlayerIdx];
-	(otherPlayerCol.childNodes[1] as HTMLHeadElement).innerHTML = otherPlayer.name;
-	(otherPlayerCol.childNodes[3] as HTMLHeadElement).innerHTML = otherPlayer.score
-		? otherPlayer.score.toString()
-		: "watiing...";
 
-	populateColumnWords(sortedBoardWords, wordsFoundByPlayers, otherPlayer, otherPlayerCol);
+	const otherPlayer = Object.values(otherPlayers)[otherPlayerIdx];
+	if (!otherPlayer) {
+		console.warn("Other player not found");
+		return;
+	}
+
+	(otherPlayerCol.childNodes[1] as HTMLHeadElement).innerHTML = otherPlayer.name;
+	(otherPlayerCol.childNodes[3] as HTMLHeadElement).innerHTML =
+		otherPlayer.score !== undefined ? otherPlayer.score.toString() : "waiting...";
+
+	populateColumnWords(otherPlayer, otherPlayerCol);
 }
 
-function populateColumnWords(
-	sortedBoardWords: string[],
-	wordsFoundByPlayers: Set<string>,
-	player: ScoredPlayer,
-	scoreCol: HTMLElement
-) {
+function populateColumnWords(player: ScoredPlayer, scoreCol: HTMLElement) {
 	for (const word of sortedBoardWords) {
 		if (wordsFoundByPlayers.has(word)) {
 			const wordRow = document.createElement("span");
@@ -89,6 +138,20 @@ function populateColumnWords(
 			wordRow.append(wordRowWord, wordRowScore);
 			(scoreCol.childNodes[5] as HTMLDivElement).appendChild(wordRow);
 		}
+	}
+}
+
+async function checkForUpdatedSession() {
+	const res = await getSessionInfo(SESSION_ID ?? "");
+	const newSession: SessionView = await res.json();
+
+	if (!session) {
+		console.error("Session not initialized yet!");
+		return;
+	}
+
+	if (!isEqual(newSession.scoredUsers, session.scoredUsers)) {
+		location.reload();
 	}
 }
 
