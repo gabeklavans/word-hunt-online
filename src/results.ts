acf9cb567fe33d45026b41fc1ec89a7e3a49cd8b
Gabe <gabeklav@fastmail.com>
2023-06-04 12:10:18 -0400

    Better unified scrolling board

--- src/results.ts
+++ src/results.ts
@@ -176,6 +176,9 @@ function populateColumnWords(scoreCol: HTMLDivElement) {
 }
 
 function updateScoreColWords(player: ScoredPlayer, scoreCol: HTMLDivElement) {
+	// prevent funky lingering scroll
+	(document.getElementById("scoreWindow") as HTMLDivElement).scrollTop = 0;
+
 	for (const wordScoreRowSpan of scoreCol.children) {
 		const [word, score] = Array.from(wordScoreRowSpan.children).map((child) => child.innerHTML);
 
